aa16329b5293a596ce78323ba964ee8b8146b053
Alex Jordan <alexander3223098@gmail.com>
2013-02-16 19:56:40 -0800

    allow dist directory for publishing

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+$("#puzzlelaunch").click(function(){$("#gamemenu").hide(),$("footer").hide(),Crafty.init(),Crafty.e("2D, DOM, Color, Floor").color("green").attr({w:500,h:25,y:100}),Crafty.e("2D, Gravity, Twoway, Color, DOM").twoway(3,5).color("red").attr({w:50,h:50}).gravity("Floor")});
