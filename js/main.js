fd562995d4d421e0b23f4c32de1c2b04e5314e17
Alex Jordan <alexander3223098@gmail.com>
2013-02-16 16:16:01 -0800

    move to Yeoman-based project

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function initializepuzzlelevels() {
-	console.log("switching to level screen");
-	var levelselect = document.getElementById("levelselect");
-	var gamemenu = document.getElementById("gamemenu");
-	gamemenu.hidden = "true";
-	levelselect.removeAttribute("hidden");
-	console.log("done switching to level screen");
-}
