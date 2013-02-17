fdd0a223c6070807b2eae1e8c0ed3f96a099b7b2
Alex Jordan <alexander3223098@gmail.com>
2013-02-16 21:27:47 -0800

    fix some grunt/jshint errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,14 @@
 //bind the event handler
-$("#puzzlelaunch").click(function() {
-	$("#gamemenu").hide();
-	$("footer").hide();
+$('#puzzlelaunch').click(function () {
+	$('#gamemenu').hide();
+	$('footer').hide();
 	Crafty.init();	
-	var floor = Crafty.e("2D, DOM, Color, Floor")
-		.color("green")
+	var floor = Crafty.e('2D, DOM, Color, Floor')
+		.color('green')
 		.attr({w: 500, h: 25, y: 100});
-	var mainChar = Crafty.e("2D, Gravity, Twoway, Color, DOM")
+	var mainChar = Crafty.e('2D, Gravity, Twoway, Color, DOM')
 		.twoway(3, 5)
-		.color("red")
+		.color('red')
 		.attr({w: 50, h: 50})
-		.gravity("Floor");
+		.gravity('Floor');
 });
